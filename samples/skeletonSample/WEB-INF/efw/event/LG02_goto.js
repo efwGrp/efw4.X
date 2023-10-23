2f9bf92b0d282b8a8c4b1f772e2beaf1b78efcfc
Chang Kejun <changkejun@hotmail.com>
2023-10-23 10:41:40 +0900

    update help

--- samples/skeletonSample/WEB-INF/efw/event/LG02_goto.js
+++
@@ -1,7 +0,0 @@
-var LG02_goto={};
-LG02_goto.name="メニュー 画面遷移";
-LG02_goto.paramsFormat={"page":null};
-LG02_goto.fire=function(params){
-    return (new Result())
-    .navigate(params["page"]);
-};
