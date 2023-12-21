875aa808948e1cb7f595f9b4b336fdeb1a710863
Chang Kejun <changkejun@hotmail.com>
2023-12-21 17:13:12 +0900

    4.07.010

--- src/efw/framework.java
+++ src/efw/framework.java
@@ -33,7 +33,7 @@ public final class framework {
 	/**
 	 * バージョンを表す。
 	 */
-	public static final String version="4.07.009";// change it when releasing jar.
+	public static final String version="4.07.010";// change it when releasing jar.
 	/**
 	 * webHome
 	 */
