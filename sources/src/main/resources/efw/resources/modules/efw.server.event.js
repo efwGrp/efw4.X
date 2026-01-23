ad5143982511f4518b1ca64e7932684f4ab9c731
Chang Kejun <changkejun@hotmail.com>
2026-01-23 12:51:51 +0900

    Update efw.server.event.js

--- sources/src/main/resources/efw/resources/modules/efw.server.event.js
+++ sources/src/main/resources/efw/resources/modules/efw.server.event.js
@@ -1,145 +1,162 @@
 "use strict";
 /**** efw4.X Copyright 2025 efwGrp ****/
 /**
- * The class to operate event.
- * 
+ * イベントの実行と管理を行うためのクラス。
+ * サーバーサイドのビジネスロジック（イベント）の呼び出しを制御します。
  * @author Chang Kejun
+ * @constructor
  */
 function EfwServerEvent() {
 };
+
 /**
- * The function to fire another event in an event firing.
- * 
- * @param {String}
- *            eventId: required<br>
- * @param {Object}
- *            params: optional<br>
- *            {param1:value1,param2:value2,...}<br>
- * @param {String}
- *            server: optional<br>
- *            The url of cors connections to another web server application constructed by Efw.<br>
- *            http://127.0.0.1:8080/efw<br>
- * @returns {Result}
+ * 指定されたイベントを実行（fire）します。
+ * 同一サーバー内のイベントだけでなく、別サーバー（CORS）のイベント呼び出しも可能です。
+ * @param {String} eventId - 必須。実行するイベントのID。
+ * @param {Object} [params={}] - 任意。イベントに渡すパラメータ。
+ * @param {String} [server] - 任意。別のefwサーバーのURL（例: http://127.0.0.1:8080/app）。
+ * @returns {Result} イベントの実行結果（Resultオブジェクト）。
  */
-EfwServerEvent.prototype.fire = function(eventId, params, server) {
-	if (server==undefined){
-		if (params==undefined){
-			params={};
-		}else if(typeof(params) == "string"){
-			server=params;
-			params={};
+EfwServerEvent.prototype.fire = function (eventId, params, server) {
+	if (server == undefined) {
+		if (params == undefined) {
+			params = {};
+		} else if (typeof (params) == "string") {
+			server = params;
+			params = {};
 		}
 	}
-	var result=new Result();
-	if (server==undefined){
-		var ev=this._get(eventId);
-		if (ev==null||ev.from=="file"){
-			ev=this._load(eventId);
+	var result = new Result();
+	if (server == undefined) {
+		// ローカルイベントの実行
+		var ev = this._get(eventId);
+		if (ev == null || ev.from == "file") {
+			ev = this._load(eventId);
 		}
-		result=ev.fire(params);
-	}else{
+		result = ev.fire(params);
+	} else {
+		// リモートイベントの実行（別のefwサーバーへのリクエスト）
 		var servletUrl = "efwServlet";
-		var jsonString=""+Packages.efw.event.RemoteEventManager.call(
-				server+"/"+servletUrl,
-				JSON.stringify({eventId:eventId,params:params})
-			);
-		var resultJSON=JSON.parse(jsonString);
-		if (resultJSON.actions!=null&&resultJSON.values!=null){
-			result.actions=resultJSON.actions;
-			result.values=resultJSON.values;
-		}else{
-			result=resultJSON;
+		var jsonString = "" + Packages.efw.event.RemoteEventManager.call(
+			server + "/" + servletUrl,
+			JSON.stringify({ eventId: eventId, params: params })
+		);
+		var resultJSON = JSON.parse(jsonString);
+		if (resultJSON.actions != null && resultJSON.values != null) {
+			result.actions = resultJSON.actions;
+			result.values = resultJSON.values;
+		} else {
+			result = resultJSON;
 		}
 	}
 	return result;
 };
+
 ///////////////////////////////////////////////////////////////////////////////
 
-EfwServerEvent.prototype._get = function(eventId){
+/**
+ * グローバルスコープからイベントオブジェクトを取得します。
+ * @param {String} eventId - イベントID。
+ * @returns {Object|undefined}
+ * @private
+ */
+EfwServerEvent.prototype._get = function (eventId) {
 	var g = Function('return this')();
 	return g[eventId];
 };
-EfwServerEvent.prototype._set = function(eventId, ev){
-	var g = Function('return this')();
-	g[eventId]=ev;
-};
+
 /**
- * The locker for event operating.
+ * グローバルスコープにイベントオブジェクトを設定します。
+ * @param {String} eventId - イベントID。
+ * @param {Object} ev - イベントオブジェクト。
+ * @private
+ */
+EfwServerEvent.prototype._set = function (eventId, ev) {
+	var g = Function('return this')();
+	g[eventId] = ev;
+};
+
+/**
+ * イベント操作用の排他制御ロッカー。
+ * @private
  */
 EfwServerEvent.prototype._locker = Packages.efw.script.ScriptManager.getLocker();
+
 /**
- * The function to load a event.<br>
- * If the debug mode,load event every time.<br>
- * If the release mode, load event only the first time.<br>
- * If the event is from resource,do not reload it.<br>
- * @param {String}
- *            eventId: required<br>
- * @returns {EventInfo}
+ * イベントファイルをロードし、メモリ上に展開します。
+ * デバッグモードの場合、ファイルの更新日時をチェックして自動的に再ロードを行います。
+ * @param {String} eventId - 必須。ロードするイベントID。
+ * @param {Boolean} [loadingGlobal] - 内部用。global.jsのロード中かどうか。
+ * @returns {EventInfo|null} ロードされたイベント情報。
+ * @private
  */
-EfwServerEvent.prototype._load = function(eventId,loadingGlobal){
-	//if the global.js is not exists,warning log.
-	if (loadingGlobal){
-		if (!absfile.exists(_eventfolder + "/" + eventId + ".js")){
-			Packages.efw.framework.initWLog( eventId + ".js is not found.");
+EfwServerEvent.prototype._load = function (eventId, loadingGlobal) {
+	// global.jsが存在しない場合の警告
+	if (loadingGlobal) {
+		if (!absfile.exists(_eventfolder + "/" + eventId + ".js")) {
+			Packages.efw.framework.initWLog(eventId + ".js is not found.");
 			return null;
 		}
 	}
-	//--------------------
+
 	/**
-	 * This function to set eventinfo about service
+	 * イベントのサービス設定（同時実行制限など）を反映します。
 	 */
-	function setService(eventId,ev){
-		if (ev.service!=null){
-			if(ev.service.max!=null && ev.service.max>-1){
-				ev.semaphore=Packages.efw.script.ScriptManager.getSemaphore(eventId,ev.service.max);
+	function setService(eventId, ev) {
+		if (ev.service != null) {
+			if (ev.service.max != null && ev.service.max > -1) {
+				// セマフォによる最大同時実行数の制限を設定
+				ev.semaphore = Packages.efw.script.ScriptManager.getSemaphore(eventId, ev.service.max);
 			}
-		}else{
-			ev.semaphore=null;
+		} else {
+			ev.semaphore = null;
 		}
 	}
-	//--------------------
+
 	try {
 		this._locker.lock();
-		//if the event hasnot be loaded, load it.
-		if(!this._get(eventId)){
+		// イベントが未ロードの場合はロード
+		if (!this._get(eventId)) {
 			load(_eventfolder + "/" + eventId + ".js");
-			var ev=this._get(eventId);
-			if (ev){
-				ev.lastModified =""+absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
-				if(eventId!="global")setService(eventId,ev);
-				ev.from="file";//from is checked in fire event, so it must be the last step.
+			var ev = this._get(eventId);
+			if (ev) {
+				ev.lastModified = "" + absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
+				if (eventId != "global") setService(eventId, ev);
+				ev.from = "file";
 			}
-		}else if (_isdebug){
-			var org=this._get(eventId);
-			var orgLastModified=""+org.lastModified;
-			var evLastModified=""+absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
-			if (orgLastModified!=evLastModified){
-				this._set(eventId,null);
+		} else if (_isdebug) {
+			// デバッグモード時はファイルの更新を確認して再ロード
+			var org = this._get(eventId);
+			var orgLastModified = "" + org.lastModified;
+			var evLastModified = "" + absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
+			if (orgLastModified != evLastModified) {
+				this._set(eventId, null);
 				load(_eventfolder + "/" + eventId + ".js");
-				var ev=this._get(eventId);
-				if (ev){
-					ev.lastModified =absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
-					setService(eventId,ev);
-					ev.from="file";//from is checked in fire event, so it must be the last step.
+				var ev = this._get(eventId);
+				if (ev) {
+					ev.lastModified = absfile.get(_eventfolder + "/" + eventId + ".js").lastModified;
+					setService(eventId, ev);
+					ev.from = "file";
 				}
 			}
 		}
-	}catch(e){
-		if (e instanceof Error)e=""+e;
-		if (loadingGlobal){
-			Packages.efw.framework.initSLog("global.js cannot be loaded.",e);
-		}else{
+	} catch (e) {
+		if (e instanceof Error) e = "" + e;
+		if (loadingGlobal) {
+			Packages.efw.framework.initSLog("global.js cannot be loaded.", e);
+		} else {
 			Packages.efw.framework.runtimeSLog(e);
 		}
-		this._set(eventId,null);
+		this._set(eventId, null);
 	} finally {
 		this._locker.unlock();
 	}
 	return this._get(eventId);
 };
+
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * create instances.
+ * イベント操作用グローバルインスタンス。
+ * @global
  */
 var event = new EfwServerEvent();
-
