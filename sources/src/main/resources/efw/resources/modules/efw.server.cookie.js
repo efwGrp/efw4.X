c16d0820f946912d5eb7d50d408b0afa50709c72
Chang Kejun <changkejun@hotmail.com>
2026-01-23 12:45:55 +0900

    Update efw.server.cookie.js

--- sources/src/main/resources/efw/resources/modules/efw.server.cookie.js
+++ sources/src/main/resources/efw/resources/modules/efw.server.cookie.js
@@ -1,41 +1,43 @@
 "use strict";
 /**** efw4.X Copyright 2025 efwGrp ****/
 /**
- * The class to operate cookie.
- * 
+ * クッキー（Cookie）を操作するためのクラス。
+ * クライアントのブラウザにデータを一時保持または取得するために使用します。
  * @author Chang Kejun
+ * @constructor
  */
 function EfwServerCookie() {
 };
+
 /**
- * The function to get data from cookie.
- * 
- * @param {String}
- *            key: the cookie key.
- * @returns {String}
+ * 指定されたキーに対応する値をクッキーから取得します。
+ * @param {String} key - 取得したいクッキーのキー名。
+ * @returns {String|null} クッキーの値。存在しない場合は null を返します。
  */
-EfwServerCookie.prototype.get = function(key) {
-	var value=Packages.efw.cookie.CookieManager.get(key);
+EfwServerCookie.prototype.get = function (key) {
+	var value = Packages.efw.cookie.CookieManager.get(key);
 	if (value == null) {
 		value = null;
 	} else {
+		// JavaのStringオブジェクトをJavaScriptの文字列に明示的に変換
 		value = "" + value;
 	}
 	return value;
 };
+
 /**
- * The function to set data in cookie.
- * 
- * @param {String}
- *            key: the cookie key.
- * @param {String}
- *            value: the data you want to set in cookie.
+ * 指定されたキーと値をクッキーに設定（保存）します。
+ * @param {String} key - 設定するクッキーのキー名。
+ * @param {String} value - 設定する値。内部で文字列に変換されます。
  */
-EfwServerCookie.prototype.set = function(key, value) {
-	Packages.efw.cookie.CookieManager.set(key, ""+value);
+EfwServerCookie.prototype.set = function (key, value) {
+	// JavaScriptの値を文字列化してJavaマネージャーに渡す
+	Packages.efw.cookie.CookieManager.set(key, "" + value);
 };
+
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * create instances.
+ * クッキー操作用グローバルインスタンス。
+ * @global
  */
 var cookie = new EfwServerCookie();
