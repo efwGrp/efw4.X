4c54f612ec815369c5ac6f86b7fb211e6f780438
Chang Kejun <changkejun@hotmail.com>
2026-01-23 12:50:30 +0900

    Update efw.server.db.js

--- sources/src/main/resources/efw/resources/modules/efw.server.db.js
+++ sources/src/main/resources/efw/resources/modules/efw.server.db.js
@@ -1,154 +1,139 @@
 "use strict";
 /**** efw4.X Copyright 2025 efwGrp ****/
 /**
- * The class to operate Database.
- * 
+ * データベース操作を行うためのクラス。
  * @author Chang Kejun
+ * @constructor
  */
-function EfwServerDb() {};
+function EfwServerDb() { };
+
 /**
- * The class to execute insert update delete sql in database.
- * <br>
- * db.change(groupId,sqlId,params)<br>
- * db.change(groupId,sqlId,params,jdbcResourceName)<br>
- * db.change(sql)<br>
- * db.change(sql,jdbcResourceName)<br>
- * <br>
- * @param {String}
- *            groupId<br>
- * @param {String}
- *            sqlId<br>
- * @param {Object}
- *            params<br>
- *            {param1:value1,param2:value2,...}<br>
- * @param {String}
- *            jdbcResourceName<br>
- * @param {String}
- *            sql<br>
- * @returns {Number}
+ * データの登録・更新・削除（INSERT/UPDATE/DELETE）を実行します。
+ * * @example
+ * db.change(groupId, sqlId, params);
+ * db.change(groupId, sqlId, params, jdbcResourceName);
+ * db.change(sql);
+ * db.change(sql, jdbcResourceName);
+ * * @param {String} groupId - SQLグループID。
+ * @param {String} sqlId - SQL ID。
+ * @param {Object} params - SQLパラメータ {key: value, ...}。
+ * @param {String} [jdbcResourceName] - 使用するJDBCリソース名。
+ * @returns {Number} 影響を受けた行数。
  */
-EfwServerDb.prototype.change = function(groupId, sqlId, params, jdbcResourceName) {
-	var count=0;
-	if (params==undefined){
-		var innerSql=groupId;//first param
-		var innerJdbcResourceName=sqlId;//second param
+EfwServerDb.prototype.change = function (groupId, sqlId, params, jdbcResourceName) {
+	var count = 0;
+	if (params == undefined) {
+		var innerSql = groupId;//first param
+		var innerJdbcResourceName = sqlId;//second param
 		count = this._executeUpdate({
-			"jdbcResourceName" : innerJdbcResourceName,
-			"sql" : innerSql
+			"jdbcResourceName": innerJdbcResourceName,
+			"sql": innerSql
 		});
-	}else{
+	} else {
 		count = this._executeUpdate({
-			"jdbcResourceName" : jdbcResourceName,
-			"groupId" : groupId,
-			"sqlId" : sqlId,
-			"params" : params
+			"jdbcResourceName": jdbcResourceName,
+			"groupId": groupId,
+			"sqlId": sqlId,
+			"params": params
 		});
 	}
 	return count;
 };
+
 /**
- * The locker for master operating.
+ * マスタ操作用の排他制御用ロッカー。
+ * @private
  */
 EfwServerDb.prototype._locker = Packages.efw.script.ScriptManager.getLocker();
-EfwServerDb.prototype._masters={};
+
 /**
- * The function to operate master data in memory.
- * 
- * @param {String}
- *            masterId: required<br>
- * @param {Boolean}
- *            reload<br>
- * @param {String}
- *            jdbcResourceName<br>
- * @returns {Record}
+ * メモリ内にキャッシュされたマスタデータ。
+ * @private
  */
-EfwServerDb.prototype.master=function(masterId, reload, jdbcResourceName) {
+EfwServerDb.prototype._masters = {};
+
+/**
+ * メモリ上のマスタデータを操作します。
+ * 指定されたテーブルの内容を全件取得し、メモリにキャッシュします。
+ * * @param {String} masterId - マスタ（テーブル）名。
+ * @param {Boolean} [reload=false] - 強制的に再読み込みするかどうか。
+ * @param {String} [jdbcResourceName] - JDBCリソース名。
+ * @returns {Record} マスタデータのRecordオブジェクト。
+ */
+EfwServerDb.prototype.master = function (masterId, reload, jdbcResourceName) {
 	this._locker.lock();
 	var values;
-	if (jdbcResourceName==undefined && reload==undefined){
-		jdbcResourceName="";
-		reload=false;
+	if (jdbcResourceName == undefined && reload == undefined) {
+		jdbcResourceName = "";
+		reload = false;
 	}
-	if (jdbcResourceName==undefined && typeof(reload) == "string"){
-		jdbcResourceName=reload;
-		reload=false;
+	if (jdbcResourceName == undefined && typeof (reload) == "string") {
+		jdbcResourceName = reload;
+		reload = false;
 	}
-	if (jdbcResourceName==undefined){
-		jdbcResourceName="";
+	if (jdbcResourceName == undefined) {
+		jdbcResourceName = "";
 	}
-		
+
 	try {
 		if (reload == true) {
-			this._masters[jdbcResourceName+":"+masterId] = this._executeQuery({
-						"sql" : "select * from " + masterId,
-						"jdbcResourceName":jdbcResourceName,
-					});
+			this._masters[jdbcResourceName + ":" + masterId] = this._executeQuery({
+				"sql": "select * from " + masterId,
+				"jdbcResourceName": jdbcResourceName,
+			});
 		} else if (EfwServerDb.prototype._masters[masterId] == null) {
-			this._masters[jdbcResourceName+":"+masterId] = this._executeQuery({
-						"sql" : "select * from " + masterId,
-						"jdbcResourceName":jdbcResourceName,
-					});
+			this._masters[jdbcResourceName + ":" + masterId] = this._executeQuery({
+				"sql": "select * from " + masterId,
+				"jdbcResourceName": jdbcResourceName,
+			});
 		}
-		values = this._masters[jdbcResourceName+":"+masterId];
+		values = this._masters[jdbcResourceName + ":" + masterId];
 	} finally {
 		this._locker.unlock();
 	}
 	return new Record(values);
 };
+
 /**
- * The function to execute select sql in database.<br>
- * <br>
- * select(groupId,sqlId,params)<br>
- * select(groupId,sqlId,params,jdbcResourceName)<br>
- * select(sql)<br>
- * select(sql,jdbcResourceName)<br>
- * <br>
- * @param {String}
- *            groupId<br>
- * @param {String}
- *            sqlId<br>
- * @param {Object}
- *            params<br>
- *            {param1:value1,param2:value2,...}<br>
- * @param {String}
- *            jdbcResourceName<br>
- * @param {String}
- *            sql<br>
- * @returns {Record}
+ * データの検索（SELECT）を実行します。
+ * * @example
+ * db.select(groupId, sqlId, params);
+ * db.select(groupId, sqlId, params, jdbcResourceName);
+ * db.select(sql);
+ * db.select(sql, jdbcResourceName);
+ * * @param {String} groupId - SQLグループID。
+ * @param {String} sqlId - SQL ID、またはJDBCリソース名。
+ * @param {Object} params - SQLパラメータ。
+ * @param {String} [jdbcResourceName] - JDBCリソース名。
+ * @returns {Record} 検索結果を保持するRecordオブジェクト。
  */
-EfwServerDb.prototype.select =function(groupId, sqlId, params, jdbcResourceName) {
+EfwServerDb.prototype.select = function (groupId, sqlId, params, jdbcResourceName) {
 	var values;
-	if (params==undefined){
-		var innerSql=groupId;//first param
-		var innerJdbcResourceName=sqlId;//second param
+	if (params == undefined) {
+		var innerSql = groupId;//first param
+		var innerJdbcResourceName = sqlId;//second param
 		values = this._executeQuery({
-			"jdbcResourceName" : innerJdbcResourceName,
-			"sql" : innerSql
+			"jdbcResourceName": innerJdbcResourceName,
+			"sql": innerSql
 		});
-	}else{
+	} else {
 		values = this._executeQuery({
-			"jdbcResourceName" : jdbcResourceName,
-			"groupId" : groupId,
-			"sqlId" : sqlId,
-			"params" : params
+			"jdbcResourceName": jdbcResourceName,
+			"groupId": groupId,
+			"sqlId": sqlId,
+			"params": params
 		});
 	}
 	return new Record(values);
 };
+
 ///////////////////////////////////////////////////////////////////////////////
+
 /**
- * The function to execute select sql.
- * 
- * @param executionParams
- *            <br>{ <br>
- *            groupId:String,//required<br>
- *            sqlId:String,//optional<br>
- *            sql:String,//optional<br>
- *            params:{param1:value1,param2:value2,...}//required<br>
- *            jdbcResourceName:String,//optional<br> }<br>
- * @returns {null | Array}
+ * 検索クエリをJavaレイヤーで実行し、JavaScriptの配列に変換します。
+ * @private
  */
-EfwServerDb.prototype._executeQuery = function(executionParams) {
+EfwServerDb.prototype._executeQuery = function (executionParams) {
 	var jdbcResourceName = executionParams.jdbcResourceName;
 	if (jdbcResourceName == undefined || jdbcResourceName == null)
 		jdbcResourceName = "";
@@ -157,19 +142,19 @@ EfwServerDb.prototype._executeQuery = function(executionParams) {
 	var sql = executionParams.sql;
 	var aryParam = executionParams.params;
 	var params = new java.util.HashMap();
-	for ( var key in aryParam) {
+	for (var key in aryParam) {
 		var vl = aryParam[key];
-		if (null == vl ||(typeof(vl) == "string" && vl == "")){
+		if (null == vl || (typeof (vl) == "string" && vl == "")) {
 			vl = null;
-		}else if (vl.getTime){
+		} else if (vl.getTime) {
 			vl = new java.sql.Timestamp(vl.getTime());
 		}
 		params.put(key, vl);
 	}
-	var db=Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
-	if (db==null){
+	var db = Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
+	if (db == null) {
 		Packages.efw.db.DatabaseManager.open(jdbcResourceName);
-		db=Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
+		db = Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
 	}
 	var rs;
 	if (sqlId != null) {
@@ -181,87 +166,65 @@ EfwServerDb.prototype._executeQuery = function(executionParams) {
 	}
 	var ret = [];
 	var meta = rs.getMetaData();
-	var parseValue = function(vl) {
+	var parseValue = function (vl) {
 		var value = vl;
 		if (value == null) {
 			//値がnullの場合処理を飛ばす
-		}else{
-			var valueType=typeof value;
-			//if (valueType == "string") {
-				//以下タイプは自動的に文字と見なす
-				//java.lang.String
-			//}else if (valueType == "number") {
-				//以下タイプは自動的に数字と見なす
-				//java.lang.Byte
-				//java.lang.Double
-				//java.lang.Float
-				//java.lang.Integer
-				//java.lang.Short
-			//}else if (valueType == "boolean") {
-				//以下タイプは自動的にブールと見なす
-				//java.lang.Boolean
-			if ((valueType == "object"||valueType =="function") && value.getClass) {
-				var clsName=value.getClass().getName();
-				if(clsName=="java.lang.Long"
-						|| clsName=="java.math.BigDecimal"){
-					value = 0 + new Number(value);//new Numberはdebug関数を付けていないため+0にする
+		} else {
+			var valueType = typeof value;
+			if ((valueType == "object" || valueType == "function") && value.getClass) {
+				var clsName = value.getClass().getName();
+				if (clsName == "java.lang.Long"
+					|| clsName == "java.math.BigDecimal") {
+					value = 0 + new Number(value);
 				} else if (clsName == "java.sql.Date"
-						|| clsName == "java.sql.Time"
-						|| clsName == "java.sql.Timestamp"
-						|| clsName == "java.util.Date") {
+					|| clsName == "java.sql.Time"
+					|| clsName == "java.sql.Timestamp"
+					|| clsName == "java.util.Date") {
 					var dt = new Date();
 					dt.setTime(value.getTime());
 					value = dt;
-				} else if (clsName == "oracle.sql.TIMESTAMP"){
+				} else if (clsName == "oracle.sql.TIMESTAMP") {
 					var dt = new Date();
 					dt.setTime(value.timestampValue().getTime());
 					value = dt;
-				} else if (clsName == "java.time.LocalDateTime"){
+				} else if (clsName == "java.time.LocalDateTime") {
 					var dt = new Date();
 					dt.setTime(java.sql.Timestamp.valueOf(value).getTime());
 					value = dt;
 				} else {
-					var sValue=""+value;
-					if (sValue.length>50){
-						sValue=sValue.substr(0,50)+"...";
+					var sValue = "" + value;
+					if (sValue.length > 50) {
+						sValue = sValue.substr(0, 50) + "...";
 					}
-					// you should do something if the comment is printed out.
 					Packages.efw.framework.runtimeWLog("[" + sValue + "] is an instance of " + value.getClass().getName()
-							+ " which is treated as a string in efw.");
-					value=""+value;//change unknown type to string.
+						+ " which is treated as a string in efw.");
+					value = "" + value;
 				}
-			//}else{//javascript objectの場合//db処理の場合存在しない
 			}
 		}
 		return value;
 	};
 
-	while (rs.next()) {// change recordset to javascript array
+	while (rs.next()) {
 		var rsdata = {};
 		var maxColumnCount = meta.getColumnCount();
 		for (var j = 1; j <= maxColumnCount; j++) {
-			var key = (""+meta.getColumnName(j)).toLowerCase();
+			var key = ("" + meta.getColumnName(j)).toLowerCase();
 			rsdata[key] = parseValue(rs.getObject(key));
 		}
 		ret.push(rsdata);
 	}
 	rs.close();
-	// close query to free handle
 	Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName).closeQuery();
 	return ret;
 };
+
 /**
- * The function to execute insert update delete sql.
- * @param executionParams
- *            <br>{ <br>
- *            groupId:String,//required<br>
- *            sqlId:String,//optional<br>
- *            sql:String,//optional<br>
- *            params:{param1:value1,param2:value2,...}//required<br>
- *            jdbcResourceName:String,//optional<br> }<br>
- * @returns {Number}
+ * 更新クエリ（INSERT/UPDATE/DELETE）をJavaレイヤーで実行します。
+ * @private
  */
-EfwServerDb.prototype._executeUpdate = function(executionParams) {
+EfwServerDb.prototype._executeUpdate = function (executionParams) {
 	var jdbcResourceName = executionParams.jdbcResourceName;
 	if (jdbcResourceName == undefined || jdbcResourceName == null)
 		jdbcResourceName = "";
@@ -270,19 +233,19 @@ EfwServerDb.prototype._executeUpdate = function(executionParams) {
 	var sql = executionParams.sql;
 	var aryParam = executionParams.params;
 	var params = new java.util.HashMap();
-	for ( var key in aryParam) {
+	for (var key in aryParam) {
 		var vl = aryParam[key];
-		if (null == vl ||(typeof(vl) == "string" && vl == "")){
+		if (null == vl || (typeof (vl) == "string" && vl == "")) {
 			vl = null;
-		}else if (vl.getTime){
+		} else if (vl.getTime) {
 			vl = new java.sql.Timestamp(vl.getTime());
 		}
 		params.put(key, vl);
 	}
-	var db=Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
-	if (db==null){
+	var db = Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
+	if (db == null) {
 		Packages.efw.db.DatabaseManager.open(jdbcResourceName);
-		db=Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
+		db = Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName);
 	}
 	if (sqlId != null)
 		return db.executeUpdate(groupId, sqlId, params);
@@ -290,48 +253,56 @@ EfwServerDb.prototype._executeUpdate = function(executionParams) {
 		return db.executeUpdateSql(sql);
 	return 0;
 };
+
 /**
- * The function to commit.
- * @param {String} jdbcResourceName
+ * 指定したJDBCリソースに対してコミットを実行します。
+ * @param {String} [jdbcResourceName]
+ * @private
  */
-EfwServerDb.prototype._commit = function(jdbcResourceName) {
+EfwServerDb.prototype._commit = function (jdbcResourceName) {
 	if (jdbcResourceName == undefined || jdbcResourceName == null)
 		jdbcResourceName = "";
 	Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName).commit();
 };
+
 /**
- * The function to rollback.
- * @param {String} jdbcResourceName
+ * 指定したJDBCリソースに対してロールバックを実行します。
+ * @param {String} [jdbcResourceName]
+ * @private
  */
-EfwServerDb.prototype._rollback = function(jdbcResourceName) {
+EfwServerDb.prototype._rollback = function (jdbcResourceName) {
 	if (jdbcResourceName == undefined || jdbcResourceName == null)
 		jdbcResourceName = "";
 	Packages.efw.db.DatabaseManager.getDatabase(jdbcResourceName).rollback();
 };
 
 /**
- * The function to commit all database.
+ * すべてのデータベース接続に対して一括コミットを実行します。
+ * @private
  */
-EfwServerDb.prototype._commitAll = function() {
+EfwServerDb.prototype._commitAll = function () {
 	Packages.efw.db.DatabaseManager.commitAll();
 };
 
 /**
- * The function to rollback all database.
+ * すべてのデータベース接続に対して一括ロールバックを実行します。
+ * @private
  */
-EfwServerDb.prototype._rollbackAll = function() {
+EfwServerDb.prototype._rollbackAll = function () {
 	Packages.efw.db.DatabaseManager.rollbackAll();
 };
 
-
 /**
- * The function to close all database.
+ * すべてのデータベース接続を閉じます。
+ * @private
  */
-EfwServerDb.prototype._closeAll = function() {
+EfwServerDb.prototype._closeAll = function () {
 	Packages.efw.db.DatabaseManager.closeAll();
 };
+
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * create instances.
+ * データベース操作用グローバルインスタンス。
+ * @global
  */
 var db = new EfwServerDb();
