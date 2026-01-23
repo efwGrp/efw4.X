5097d373ad584118f3b65136452f29a1bc295c35
Chang Kejun <changkejun@hotmail.com>
2026-01-23 13:13:51 +0900

    Update efw.server.properties.js

--- sources/src/main/resources/efw/resources/modules/efw.server.properties.js
+++ sources/src/main/resources/efw/resources/modules/efw.server.properties.js
@@ -1,44 +1,51 @@
 "use strict";
 /**** efw4.X Copyright 2025 efwGrp ****/
 /**
- * The class to operate efw.properties file.
- * 
- * @author Chang Kejun
+ * efw.propertiesファイルを操作するためのクラス。
+ * サーバーの動作設定や環境変数を動的に取得します。
+ * * @author Chang Kejun
+ * @constructor
  */
 function EfwServerProperties() {
 };
+
 /**
- * The function to get String value from efw.properties file.
- * 
- * @param {String}
- *            key: required<br>
- * @param {String}
- *            defaultValue: optional<br>
- * @returns {any}
+ * efw.propertiesファイルから指定されたキーの値を取得します。
+ * defaultValueが指定されている場合、その型に合わせて自動的にキャストします。
+ * * @param {String} key - 必須。取得したいプロパティのキー。
+ * @param {any} [defaultValue] - 任意。キーが存在しない場合のデフォルト値。
+ * この引数の型（String/Number/Boolean）に基づいて戻り値が変換されます。
+ * @returns {any} プロパティの値。型はdefaultValueに準拠します。
  */
-EfwServerProperties.prototype.get = function(key, defaultValue) {
+EfwServerProperties.prototype.get = function (key, defaultValue) {
 	var dv;
 	if (defaultValue == null) {
 		dv = null;
 	} else {
 		dv = defaultValue + "";
 	}
-	var value=""+Packages.efw.properties.PropertiesManager.getProperty(key, dv);
-	if(defaultValue==null){
+
+	// JavaのPropertiesManagerから値を取得
+	var value = "" + Packages.efw.properties.PropertiesManager.getProperty(key, dv);
+
+	// デフォルト値の型に応じたキャスト処理
+	if (defaultValue == null) {
 		return value;
-	}else if(typeof defaultValue=="string"){
+	} else if (typeof defaultValue == "string") {
 		return value;
-	}else if(typeof defaultValue=="number"){
-		return 0+new Number(value);
-	}else if(typeof defaultValue=="boolean"){
+	} else if (typeof defaultValue == "number") {
+		return 0 + new Number(value);
+	} else if (typeof defaultValue == "boolean") {
+		// Javaのパース処理を利用して正確に変換
 		return true && java.lang.Boolean.parseBoolean(value);
-	}else{
+	} else {
 		return value;
 	}
 };
+
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * create instances.
+ * プロパティ操作用グローバルインスタンス。
+ * @global
  */
 var properties = new EfwServerProperties();
-
